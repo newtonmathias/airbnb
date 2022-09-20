049d3ee2c327d58a6d0bd4d31f86eeb48ca37ffb
newton mathias <newtymathias@gmail.com>
2022-09-20 14:47:24 +0300

    banner

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,10 @@
 import Image from 'next/image';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { 
+  MagnifyingGlassIcon,
+  GlobeAltIcon, 
+  Bars3Icon, 
+  UserCircleIcon 
+} from '@heroicons/react/24/solid'
 
 function Header() {
   return (
@@ -15,13 +20,25 @@ function Header() {
         </div>
 
         {/* Middle */}
-        <div>
-          <input type="text" placeholder="Start your search" />
-          <MagnifyingGlassIcon className='h-8 bg-red-400 text-white rounded-full p-2' />
+        <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+          <input
+           className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' 
+           type="text" 
+           placeholder="Start your search" 
+           />
+          <MagnifyingGlassIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
         </div>
 
         {/* Right */}
-        <div></div>
+        <div className='flex items-center space-x-4 justify-end text-gray-500'>
+          <p className='hidden md:inline  cursor-pointer'>Become a host</p>
+          <GlobeAltIcon className='h-6 cursor-pointer' />
+
+          <div className='flex items-center space-x-4 border-2 p-2 rounded-full'>
+            <Bars3Icon className='h-6' />
+            <UserCircleIcon className='h-6' />
+          </div>
+        </div>
       </header>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,10 +13,31 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header/>
-      {/* Banner */}
+      <Header />
+      <Banner />
 
+        <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+          <section className='pt-6'>
+            <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
 
+            {exploreData?.map((item) => {
+              <h1>{item.location}</h1>
+            })}
+          </section>
+        </main>
       </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').
+  then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
